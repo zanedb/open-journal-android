531541ad0ae329f635acc2a648e236ffdecb8f38
Zane <webmaster@zaneweb.org>
2017-06-26 11:59:29 -0700

    Make FloatingActionButton (FAB) link to (and create) AddNoteActivity No content yet..

--- app/src/main/java/org/openssf/openjournal/HomeActivity.java
+++ app/src/main/java/org/openssf/openjournal/HomeActivity.java
@@ -1,7 +1,10 @@
 package org.openssf.openjournal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        FloatingActionButton addNote = (FloatingActionButton) findViewById(R.id.add_note_fab);
+        addNote.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                // On click, open AddNoteActivity
+                Intent addnoteintent = new Intent(HomeActivity.this, AddNoteActivity.class);
+                startActivity(addnoteintent);
+            }
+        });
     }
 }
