dfff5711990b90e737ef0385722de9bbcd1353ab
Zane <contact@zane.sh>
2017-10-19 21:19:31 -0700

    Begin export process

--- app/src/main/java/org/openssf/openjournal/ExportActivity.java
+++ app/src/main/java/org/openssf/openjournal/ExportActivity.java
@@ -5,6 +5,8 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ExportActivity extends AppCompatActivity {
 
@@ -21,5 +23,11 @@ public class ExportActivity extends AppCompatActivity {
         } catch (android.content.ActivityNotFoundException anfe) {
             startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
         }
+    }
+
+    public void exportNotes(View view) {
+        Toast.makeText(ExportActivity.this, getString(R.string.exporting), Toast.LENGTH_SHORT).show();
+        TextView et = (TextView) findViewById(R.id.export_text);
+        et.setText(R.string.export_start_msg);
     }
 }
